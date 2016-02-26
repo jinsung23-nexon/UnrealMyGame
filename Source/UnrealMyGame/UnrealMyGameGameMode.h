b3cee2f2698538acf75d83e762efc384e6c76528
Lee Jinsung <jinsung23@nexon.co.kr>
2016-02-26 11:05:05 +0900

    프로젝트 파일 생성 (4.10, vs2013, mobile, scalable, no contents)

---
+++ Source/UnrealMyGame/UnrealMyGameGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "UnrealMyGameGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALMYGAME_API AUnrealMyGameGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
